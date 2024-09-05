d5e46363f1941d4eea12fbece12095f027abab6e
Veer Pareek <veerpareek12@gmail.com>
2024-09-04 23:12:09 -0500

    bpe.h

--- includes/tokenizer/bpe.h
+++ includes/tokenizer/bpe.h
@@ -1,17 +1,12 @@
-// This file contains the BPE (Byte Pair Encoding) tokenizer.
-
 /**
- * TikToken's BPE tokenizer is a type of byte pair encoding that is used to tokenize text into tokens.
- * It is a simple and efficient tokenization method that is used in many NLP models.
- *
- * The tokenizer works by merging the most frequent byte pairs in the text.
- * It does this by creating a frequency list of all the byte pairs in the text.
- * The most frequent byte pair is then merged and the process is repeated until the desired number of tokens is reached.
- *
- * The tokenizer uses a special byte to represent the end of a token.
+ * This file contains the BPE (Byte Pair Encoding) tokenizer.
+ * 
+ * It is based off of the paper 'Neural Machine Translation of Rare Words with Subword Units'
  */
+
 
 #ifndef BPE_H
 #define BPE_H
+
 
 #endif // BPE_H
